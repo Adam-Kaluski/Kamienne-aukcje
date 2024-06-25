2b017cb7dbe6cda615e94e9c4cc8c1945a531a6b
Adam-Kaluski <adam.kaluski12@gmail.com>
2024-06-25 19:07:11 +0200

    -

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default {
         rollupOptions: {
             input: {
                 main: './index.html',
-                mainPage: './mainPage.html',
+
             }
         }
     },
